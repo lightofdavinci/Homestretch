3cd46713ed96fd9a9ba088ac69ba756589290324
Dan Volosnikov <volosdan@yandex.ru>
2021-04-29 19:34:15 +0400

    visual draft

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="150"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-date-picker
+        v-model="date"
+        :allowed-dates="allowedDates"
+        class="mt-4"
+        min="2016-06-15"
+        max="2018-03-20"
+      ></v-date-picker>
+    </v-row>
+    <v-row justify="center">
+      <v-btn
+        elevation="5"
+        large
+      >Reset</v-btn>
+      <v-btn
+        depressed
+        color="primary"
+        large
+      >Save</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainContainer',
+
+    data: () => ({
+      date: '2018-03-02',
+    }),
+
+    methods: {
+      allowedDates: val => parseInt(val.split('-')[2], 10) % 2 === 0,
+    },
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main-container />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import MainContainer from '../components/MainContainer'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      MainContainer,
+    },
   }
-}
 </script>
